99a4904c45d0abd15fd8b2c977d2fcdd00bc84cf
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 10:14:35 +0530

    decision-making

---
+++ conditional.py
@@ -0,0 +1,10 @@
+
+name=input('enter you name >> ')
+
+print(len(name))
+
+if(len(name)>5):
+    print('Valid name it has more than 4 characters')
+    print('We shall store it in the database')
+else:
+    print('name should have atleast four characters')
