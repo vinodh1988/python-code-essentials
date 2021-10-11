dd9c22014210213489dca3e917591bc9c0e64797
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 10:20:28 +0530

    conditional-commit

--- conditional.py
+++ conditional.py
@@ -3,8 +3,16 @@ name=input('enter you name >> ')
 
 print(len(name))
 
-if(len(name)>5):
+
+if(len(name)>=5):
     print('Valid name it has more than 4 characters')
     print('We shall store it in the database')
+elif(len(name)==3 or len(name)==4):
+    print('type confirm to store it into the database >>')
+    if 'confirm'==input():
+        print('stored in the database')
+    else:
+        print('wrong input')
 else:
-    print('name should have atleast four characters')
+    print('name should have atleast three characters')
+
