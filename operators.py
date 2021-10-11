e86fc87306e8cc2eda273ff0b52f11191c0da916
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 09:44:13 +0530

    operators-commit

---
+++ operators.py
@@ -0,0 +1,35 @@
+#arithmetic operators
+
+print('12 to the power of 3',12**3)
+print('13 divided by 2',13/2)
+print('13 divided by 2 and remainder is ',13%2)
+print('13 floor division 2',13//2)
+
+#relational Operator
+
+print(12>3)
+print(12<3)
+print(12!=12)
+print(12==12)
+print(12>=12)
+print(12<=12)
+
+print("-----------------------------------")
+
+#logical operators
+
+print(12>3 and 12<90)
+print(True and False)
+print( 123 and 12)
+print( 0 and 123)
+print(True and 142)
+print( 123 or 0)
+print( 0 or 545)
+print(12>23 or False)
+print(not True)
+print(not 123)
+print(not 0)
+print(not 234.35)
+print(not "Jay")
+print(not "")
+print(not None)
