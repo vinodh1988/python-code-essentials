90858e9391f86384f0c2bfdd09aa4f6991ba58bf
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 09:24:40 +0530

    data-types

---
+++ variables.py
@@ -0,0 +1,15 @@
+a=20
+b='raj'
+c=True
+d=23.32
+
+print(a,b,c,d)
+
+print(type(a),type(b),type(c),type(d))
+
+
+print(isinstance(a,int))
+print(isinstance(b,int))
+print(isinstance(b,str))
+print(isinstance(c,bool))
+
