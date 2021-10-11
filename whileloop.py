46f79539f397a6f886b35afb8c9c84f71c2ff2c1
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 12:01:46 +0530

    pass-commit

--- whileloop.py
+++ whileloop.py
@@ -9,3 +9,21 @@ x=1
 while x<10:
     print(x)
     x+=1
+print("--------------------------------------")
+for x in range(0,20,3):
+    if(x!=0 and x%5==0):
+        break
+    if(x==6):
+        continue
+    print(x)
+
+print("------------------------------------------------")
+
+temp=105
+
+if temp >= 98 and temp <=99:
+    pass
+elif temp >=99 and temp <= 101:
+    print('Give moderated medicine')
+else:
+    print('Take to hospital')
