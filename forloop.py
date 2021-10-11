4747e1d35e56066e29185ec3339d6b3fcb39f962
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 11:34:46 +0530

    for-loop-and-range

---
+++ forloop.py
@@ -0,0 +1,4 @@
+items=['apple','boy','cat','dog','elephant']
+
+for x in items:
+    print(x)
---
+++ range.py
@@ -0,0 +1,20 @@
+result=range(6)
+print(result)
+
+
+for x in range(10):
+    print(x)
+
+print('---------------------------------')
+
+for y in range(1,4):
+    print(y)
+
+print('---------------------------------')
+
+for z in range(1,10,2):
+    print(z)
+
+print(tuple(result))   
+print(set(result))
+print(list(range(5,50,10)))
