3169ec48f24e97edad76a0492cf87c11d3fd37a6
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-11 11:48:32 +0530

    while-loop

---
+++ morerange.py
@@ -0,0 +1,5 @@
+for x in reversed(range(1,30,2)):
+    print(x)
+
+for x in reversed([1,23,45]):
+    print(x)
---
+++ whileloop.py
@@ -0,0 +1,11 @@
+
+
+for x in range(2,100):
+    if(x%2==0 and x%3==0 and x%9==0):
+        print(x,end=":::")
+
+x=1
+
+while x<10:
+    print(x)
+    x+=1
