cf0a4ff018a4274766d7f573f3f82f3950e09f14
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-12 12:09:14 +0530

    lambda-commit

--- day_two/listtasks.py
+++ day_two/listtasks.py
@@ -5,8 +5,20 @@ numberlist=[254,2356,34634,1523,-5634,356,5633,156,-2166,36634]
 def changeSign(x):
     return x*-1
 
+def evenOrOdd(x):
+    return "Even" if x%2==0 else "Odd" 
+
+
 newlist=map(changeSign,numberlist)
+newlist2=map(evenOrOdd,numberlist)
+
+newlist3=map(lambda x:x*-1,numberlist)
+newlist4=map(lambda x:"Even" if x%2==0 else "Odd",numberlist)
 
 print(changeSign(5))
 
 print(list(newlist))
+print(list(newlist2))
+print("-----------------------------------------------")
+print(list(newlist3))
+print(list(newlist4))
