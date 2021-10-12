3b113a6f330074057aeb18b1a95a7d92a6aadbdc
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-12 11:38:07 +0530

    version3

--- day_two/closures2.py
+++ day_two/closures2.py
@@ -3,10 +3,12 @@ def outer(state):
     print('performing outer activity with ',temp)
     def inner():
         nonlocal temp
+        nonlocal state
+        state='MNCMNC'
         print('doing task utilizing outer state ',state)
         temp="XYZXYZXYZ"
         print("performing ",temp)
     inner()
     print("done with outer activity",temp)
-
+    print(state)
 outer("ABCABCABC")
