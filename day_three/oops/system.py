7350a696dc6785720e21686dd6852705a511db77
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-13 11:19:38 +0530

    multiple-inheritance

--- day_three/oops/system.py
+++ day_three/oops/system.py
@@ -6,13 +6,24 @@ class Shape:
     def _display(self):
         print('Shape is {}'.format(self.name))
 
+class Box:
+    def __init__(self,type=None):
+        self.type=type
+   
+    def _display(self):
+        print('Box is {}'.format(self.type))
 
-class Square(Shape):
+
+class Square(Box,Shape):
     __side=None
-    def __init__(self,name,side):
-        super().__init__(name)
+    def __init__(self,name,type,side):
+        #super().__init__(name)
+        Box.__init__(self,type)
+        Shape.__init__(self,name)
+
         self.__side=side
     
     def display(self):
-        self._display()
+        Shape._display(self)
+        Box._display(self)
         print('Length of square is  {}'.format(self.__side))
--- day_three/system.py
+++ day_three/system.py
@@ -1,5 +1,5 @@
 from oops import Square
 
-shape1=Square('Square', 50)
+shape1=Square('Square','Open' ,50)
 
 shape1.display()
