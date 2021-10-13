31bdbb510a0d2afa3b9143a193b5721b1ec0021b
vinodhkc@vinodhconnects.onmicrosoft.com <vinodhkc@vinodhconnects.onmicrosoft.com>
2021-10-13 09:39:15 +0530

    private-members

--- day_three/oops/student.py
+++ day_three/oops/student.py
@@ -1,9 +1,16 @@
 class Student:
-    def __init__(self,sno,name,city):
-        self.sno=sno
-        self.name=name
-        self.city=city
+    __sno = None
+    __name = None
+    __city = None
+    def __init__(self,sno,name,city,skill):
+        self.__sno=sno
+        self.__name=name
+        self.__city=city
+        self.skill=skill
     
     def display(self):
-        print("sno: {} name:{} city:{}".format(self.sno,self.name,self.city))
+        self.__callme()
+        print("sno: {} name:{} city:{} skill:{}".format(self.__sno,self.__name,self.__city,self.skill))
 
+    def __callme(self):
+        print('did you callme')
--- day_three/oopsmain.py
+++ day_three/oopsmain.py
@@ -1,7 +1,16 @@
 from oops import Student
 
-obj1=Student(1,'Rahul','Chennai')
-obj2=Student(2,'Jose','Mumbai')
+obj1=Student(1,'Rahul','Chennai','C++')
+obj2=Student(2,'Jose','Mumbai','Java')
 
 obj1.display()
 obj2.display()
+
+
+obj1.__sno=89
+obj2.__name='Rakesh'
+obj2.skill='Python'
+
+obj1.display()
+obj2.display()
+#obj1.__callme()
